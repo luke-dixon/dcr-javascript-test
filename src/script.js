f9ebf01ac36b30adc301ae6d1af408065eccf53e
Luke Dixon <luke@luke.dixon.name>
2024-06-13 03:10:50 +0100

    Add a table with the same data as the chart

--- src/script.js
+++ src/script.js
@@ -7,8 +7,12 @@ form.addEventListener("submit", (event) => {
     const chart = document.getElementById("chart");
     chart.replaceChildren();
 
+    const tableContainer = document.getElementById("table-container");
+    tableContainer.replaceChildren();
+
     const formData = new FormData(event.target, event.submitter);
     createChart(dataProcessors[formData.get("dataChoice")]);
+    addTable(dataProcessors[formData.get("dataChoice")]);
 });
 
 
@@ -18,43 +22,53 @@ const dataProcessors = {
         label: (d) => [d.name, d.population.toLocaleString("en")].join("\n"),
         value: (d) => d.population,
         group: (d) => d.region,
+        tableHeaders: ["name", "population"],
+        tableRow: (d) => [d.name, d.population],
     },
     numBorders: {
         getData: (data) => data,
         label: (d) => [d.name, d.borders.length].join("\n"),
         value: (d) => d.borders.length,
         group: (d) => d.region,
+        tableHeaders: ["name", "borders"],
+        tableRow: (d) => [d.name, d.borders.length],
     },
     numTimezones: {
         getData: (data) => data,
         label: (d) => [d.name, d.timezones.length].join("\n"),
         value: (d) => d.timezones.length,
         group: (d) => d.region,
+        tableHeaders: ["name", "timezones"],
+        tableRow: (d) => [d.name, d.timezones.length],
     },
     numLanguages: {
         getData: (data) => data,
         label: (d) => [d.name, d.languages.length].join("\n"),
         value: (d) => d.languages.length,
         group: (d) => d.region,
+        tableHeaders: ["name", "languages"],
+        tableRow: (d) => [d.name, d.languages.length],
     },
     regionNumCountries: {
         getData: (data) => {
             const regions = {};
             for (const country of data) {
                 if (country.region in regions) {
-                    regions[country.region].value++;
+                    regions[country.region].countries++;
                 } else {
                     regions[country.region] = {
                         name: country.region,
-                        value: 1,
+                        countries: 1,
                     };
                 }
             }
             return Object.values(regions);
         },
-        label: (d) => [d.name, d.value].join("\n"),
-        value: (d) => d.value,
+        label: (d) => [d.name, d.countries].join("\n"),
+        value: (d) => d.countries,
         group: (d) => d.name,
+        tableHeaders: ["name", "countries"],
+        tableRow: (d) => [d.name, d.countries],
     },
     regionNumTimezones: {
         getData: (data) => {
@@ -65,21 +79,23 @@ const dataProcessors = {
                     for (const timezone of country.timezones) {
                         timezones.add(timezone);
                     }
-                    regions[country.region].value = timezones.size;
+                    regions[country.region]["unique timezones"] = timezones.size;
                 } else {
                     const timezones = new Set(country.timezones);
                     regions[country.region] = {
                         name: country.region,
                         timezones: timezones,
-                        value: timezones.size,
+                        "unique timezones": timezones.size,
                     };
                 }
             }
             return Object.values(regions);
         },
-        label: (d) => [d.name, d.value].join("\n"),
-        value: (d) => d.value,
+        label: (d) => [d.name, d["unique timezones"]].join("\n"),
+        value: (d) => d["unique timezones"],
         group: (d) => d.name,
+        tableHeaders: ["name", "unique timezones"],
+        tableRow: (d) => [d.name, d["unique timezones"]],
     },
 };
 
@@ -104,4 +120,39 @@ async function createChart(processor) {
     document.getElementById("chart").appendChild(chart);
 }
 
+function createTable() {
+    const table = document.createElement("table");
+    const thead = document.createElement("thead");
+    const tbody = document.createElement("tbody");
+    table.appendChild(thead);
+    table.appendChild(tbody);
+    return [table, thead, tbody];
+}
+
+function createTableRow(headers, elementType) {
+    const row = document.createElement("tr");
+    for (const header of headers) {
+        const th = document.createElement(elementType);
+        row.appendChild(th);
+        th.appendChild(document.createTextNode(header));
+    }
+
+    return row;
+}
+
+async function addTable(processor) {
+    const data = processor.getData(await loadData());
+
+    const [table, thead, tbody] = createTable();
+
+    thead.appendChild(createTableRow(processor.tableHeaders, "th"));
+    for (const row of data) {
+        const rowData = processor.tableRow(row);
+        tbody.appendChild(createTableRow(rowData, "td"));
+    }
+
+    document.getElementById("table-container").appendChild(table);
+}
+
 createChart(dataProcessors.population);
+addTable(dataProcessors.population);
